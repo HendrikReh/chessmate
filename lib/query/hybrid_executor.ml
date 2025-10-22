9773422bcac44937c037f81e624a7ac4cd908d9b
Hendrik Reh <hendrik.reh@gmail.com>
2025-10-22 09:50:59 +0200

    chore: string copyright header

--- lib/query/hybrid_executor.ml
+++ lib/query/hybrid_executor.ml
@@ -1,21 +1,3 @@
-(*  Chessmate - Hybrid chess tutor combining Postgres metadata with Qdrant
-    vector search
-    Copyright (C) 2025 Hendrik Reh <hendrik.reh@blacksmith-consulting.ai>
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <https://www.gnu.org/licenses/>.
-*)
-
 (** Execute the hybrid retrieval plan by combining Postgres metadata, optional
     vector hits, and GPT-5 scores into ranked results. *)
 
--- lib/query/hybrid_executor.mli
+++ lib/query/hybrid_executor.mli
@@ -1,21 +1,3 @@
-(*  Chessmate - Hybrid chess tutor combining Postgres metadata with Qdrant
-    vector search
-    Copyright (C) 2025 Hendrik Reh <hendrik.reh@blacksmith-consulting.ai>
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <https://www.gnu.org/licenses/>.
-*)
-
 (** Coordinates fetching candidates, running agent evaluations, and scoring
     results. Rating predicates are cached per summary to avoid redundant work
     and vector scores fall back gracefully when Qdrant is unavailable. *)
